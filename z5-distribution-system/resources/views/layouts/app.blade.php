eae809c17d87c33cd2a23416ca9e11169b30364e
SimpleGuy <reeazr@gmail.com>
2025-09-11 16:54:37 +0400

    change default page title

--- z5-distribution-system/resources/views/layouts/app.blade.php
+++ z5-distribution-system/resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
+d a pre<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Dashboard') - {{ $companySettings ? $companySettings->company_name : config('app.name', 'Laravel') }}</title>
+    <title>@yield('title', 'Dashboard') - {{ $companySettings ? $companySettings->company_name : config('app.name', 'Welcome') }}</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
