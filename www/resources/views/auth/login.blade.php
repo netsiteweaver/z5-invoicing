f4b9f727045bdbc888ed7e93163fd768b0601ee2
SimpleGuy <reeazr@gmail.com>
2025-09-16 12:32:15 +0400

    add bg image to login page

--- www/resources/views/auth/login.blade.php
+++ www/resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-guest-layout :title="'Please sign in'" :bgClass="'bg-cover bg-center bg-no-repeat'" :bgStyle="'background-image: url(' . asset('images/login-bg.jpg') . ');'">
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
