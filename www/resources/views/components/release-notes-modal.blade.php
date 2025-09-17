820f103935f3d999feb4b2964847923b42222396
SimpleGuy <reeazr@gmail.com>
2025-09-18 02:28:56 +0400

    releae notes modal updated

--- www/resources/views/components/release-notes-modal.blade.php
+++ www/resources/views/components/release-notes-modal.blade.php
@@ -1,8 +1,8 @@
 <div x-data="releaseNotes()" x-init="init()">
   <div x-show="open" x-cloak class="fixed inset-0 z-50 flex items-center justify-center">
     <div class="absolute inset-0 bg-black/40" @click="dismiss()"></div>
-    <div class="relative bg-white rounded-lg shadow-xl max-w-2xl w-full mx-4">
-      <div class="px-5 py-4 border-b border-gray-200 flex items-center justify-between">
+    <div class="relative bg-white rounded-lg shadow-xl border border-gray-200 max-w-2xl w-full mx-4">
+      <div class="px-5 py-4 bg-blue-50 border-b border-blue-100 flex items-center justify-between">
         <h3 class="text-lg font-semibold text-gray-900">What's new in {{ config('app.version') }}</h3>
         <button class="text-gray-400 hover:text-gray-600" @click="dismiss()"><i class="fas fa-times"></i></button>
       </div>
@@ -17,8 +17,8 @@
           </ul>
         @endif
       </div>
-      <div class="px-5 py-4 border-t border-gray-200 flex items-center justify-end">
-        <label class="mr-auto inline-flex items-center text-sm text-gray-600">
+      <div class="px-5 py-4 bg-blue-50 border-t border-blue-100 flex items-center justify-between">
+        <label class="inline-flex items-center text-sm text-gray-600">
           <input type="checkbox" class="mr-2 rounded border-gray-300" x-model="dontShowAgain" />
           Don't show again for this version
         </label>
