13eadabb8abd1de80c968e137202cb432394f5f7
root <root@vmi2813440.contaboserver.net>
2025-10-06 06:48:36 +0200

    Remove conflicted manifest.json

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
+        'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
+        'reference' => '548c9788885b4fd1a0460e4a934648d5d92f17de',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => 'e524c44bedd869ecdbcb55357a6768bbf52ce184',
-        'name' => '__root__',
         'dev' => true,
     ),
     'versions' => array(
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
+            'reference' => '548c9788885b4fd1a0460e4a934648d5d92f17de',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => 'e524c44bedd869ecdbcb55357a6768bbf52ce184',
             'dev_requirement' => false,
         ),
     ),
